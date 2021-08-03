9497f2dcaaf0ef82c90c1409b0f86741d2ca0e2a
Diogo Miyake <diogo.miyake@xpi.com.br>
2021-08-03 17:10:02 -0300

    updated file to plot some chart

--- app/SIR.hs
+++ app/SIR.hs
@@ -1,5 +1,6 @@
-module SIR where
+{-# language OverloadedStrings #-}
 
+module SIR where
 import Graphics.Vega.Vegalite
 
 
@@ -15,6 +16,23 @@ sir beta gamma s i r = go (s/n) (i/n) (r/n)
                 si = beta*s*i
                 ir = gamma*i 
 
-plotaSIR = print 
-           $ take 10 
-           $ sir 0.2 0.1 20000 100 0
+geraDados :: Int -> [[Double]] -> [DataColumn] -> Data
+geraDados n dados = dataFromColumns []
+                    . dataColumn "Dia" (Numbers eixoX)
+                    . dataColumn "Casos" (Numbers eixoY)
+                    . dataColumns "SIR" (Strings grupos)
+                        where
+                            eixoX = concat $ take n [[x,x,x] | x <- [1 ..]]
+                            eixoY = concat $ take n dados
+                            grupos = concat $ replicate n ["S", "I", "R"] -- take n $ repeat == replicate n
+
+enc = encoding 
+      . position X [ PName "Dia", PmType Quantitative ]
+      . position Y [ PName "Casos", PmType Quantitative ]
+      . color [ MName "SIR", MmType Nominal, Mlegend [LLabelFontSize 32, LtitleFontSize 40 ] ]
+      . tooltip [ TName "Casos", TmType Quantitative ]
+
+dados = geraDados 365 $ sir 0.2 0.1 20000 100 0
+
+plotaSIR = toHtmlFile "SIR.html" 
+           $ toVegaLite [ dados [], mark Line [MStrokeWidth 10], enc [], height 800, width 800] 
